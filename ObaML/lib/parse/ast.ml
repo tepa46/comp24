8394c87c948202a05e8c7e1efa46bda9f4d73daa
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-10-19 14:59:32 +0300

    Add Ast and LocatedAst modules.

---
+++ ObaML/lib/parse/ast.ml
@@ -0,0 +1,166 @@
+(********************************Wrap********************************)
+
+module type Wrap = sig
+  type 'a t
+
+  val equal : ('a -> 'a -> bool) -> 'a t -> 'a t -> bool
+  val pp : (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a t -> unit
+  val show : (Format.formatter -> 'a -> unit) -> 'a t -> string
+  val escape : 'a t -> 'a
+end
+
+module Fiction : Wrap = struct
+  type 'a t = 'a [@@deriving eq, show { with_path = false }]
+
+  let escape x = x
+end
+
+(********************************Identifier********************************)
+
+module Id = struct
+  type t = Id of string [@@deriving eq, ord, show { with_path = false }]
+end
+
+(********************************Types********************************)
+
+module Ty (ExprW : Wrap) (DeclW : Wrap) = struct
+  module Var = struct
+    type t = Var of string [@@deriving eq, ord, show { with_path = false }]
+  end
+
+  module Expr = struct
+    module W = ExprW
+
+    type t =
+      | TVar of Var.t W.t (** ['a] *)
+      | TArrow of t W.t * t W.t (** ['a --> 'b] *)
+      | TTuple of t W.t list (** ['a * 'b * 'c] *)
+      | TConstr of t W.t list * Id.t W.t (** [int] | ['a list] | [('a, 'b) Result] *)
+    [@@deriving eq, show { with_path = false }]
+  end
+
+  module Decl = struct
+    module W = DeclW
+
+    (** [Leaf] | [Node of 'a] *)
+    type constr =
+      { name : Id.t W.t
+      ; args : Expr.t W.t
+      }
+    [@@deriving eq, show { with_path = false }]
+
+    type representation =
+      | FromTyExpr of Expr.t W.t (** [int] | [int * bool] *)
+      | FromConstrs of constr W.t list (** [Some of 'a] *)
+    [@@deriving eq, show { with_path = false }]
+
+    (** [('a, 'b) name = A of 'a | B of 'b] *)
+    type t =
+      { name : Id.t W.t (** starts with lowercase *)
+      ; params : Var.t W.t list (** [('a, 'b)] *)
+      ; representation : representation W.t (** [A of 'a | B of 'b] *)
+      }
+    [@@deriving eq, show { with_path = false }]
+  end
+end
+
+(********************************Constans********************************)
+
+module Constant (W : Wrap) = struct
+  type t =
+    | CInt of int W.t (** [52] *)
+    | CString of string W.t (** [ "hola" ] *)
+  [@@deriving eq, show { with_path = false }]
+end
+
+(********************************Patterns********************************)
+
+module Pattern (W : Wrap) (CW : Wrap) (TyExprW : Wrap) (TyDeclW : Wrap) = struct
+  module Constant = Constant (CW)
+  module Ty = Ty (TyExprW) (TyDeclW)
+
+  type t =
+    | PAny (** [_] *)
+    | PVar of Id.t W.t (** [variable] *)
+    | PConst of Constant.t W.t (** [52] | ["hola"]*)
+    | PTuple of t W.t list (** [first, second, third] *)
+    | PConstruct of Id.t W.t * t W.t option (** [Tree(left, right)] | [None] *)
+    | PType of t W.t * Ty.Expr.t W.t (** [x : int] *)
+    | POr of t W.t * t W.t (** [First | Second | Third] *)
+  [@@deriving eq, show { with_path = false }]
+end
+
+(********************************Expressions********************************)
+
+module Expr (W : Wrap) (PW : Wrap) (CW : Wrap) (TyExprW : Wrap) (TyDeclW : Wrap) = struct
+  module Pattern = Pattern (PW) (CW) (TyExprW) (TyDeclW)
+  module Constant = Pattern.Constant
+  module Ty = Pattern.Ty
+
+  type rec_flag =
+    | Recursive (** with [rec] modifier *)
+    | Nonrecursive (** without [rec] modifier *)
+  [@@deriving eq, show { with_path = false }]
+
+  (** [let _ arg1 arg2 (arg3_fst, _) = arg1 + arg2 + arg3_fst ] *)
+  type value_binding = Pattern.t W.t * t W.t [@@deriving eq, show { with_path = false }]
+
+  (** [Tree(left, right) -> left, right] *)
+  and case = Pattern.t W.t * t W.t [@@deriving eq, show { with_path = false }]
+
+  and t =
+    | EVar of Id.t W.t (** [x] *)
+    | EConst of Constant.t W.t (** [52] | ["hola"] *)
+    | ELet of rec_flag * value_binding W.t list * t W.t
+    (** [let rec p1 = e1 and p2 = e2 in e] *)
+    | EFun of value_binding W.t (** [fun x -> x] *)
+    | EFunction of case W.t list (** [function 1 -> true | _ -> false] *)
+    | EApply of t W.t * t W.t (** [f x] *)
+    | EMatch of t W.t * case W.t list (** [match x with | 1 | 2 -> true | _ -> false] *)
+    | ETuple of t W.t list (** [fst, snd, trd] *)
+    | EConstruct of Id.t W.t * t W.t option (** [Some 42] *)
+    | EIfThenElse of t W.t * t W.t * t W.t option (** [if true then false else true] *)
+  [@@deriving eq, show { with_path = false }]
+end
+
+(********************************StructureItems********************************)
+
+module StructureItem
+    (W : Wrap)
+    (EW : Wrap)
+    (PW : Wrap)
+    (CW : Wrap)
+    (TyExprW : Wrap)
+    (TyDeclW : Wrap) =
+struct
+  module Expr = Expr (EW) (PW) (CW) (TyExprW) (TyDeclW)
+  module Pattern = Expr.Pattern
+  module Constant = Expr.Constant
+  module Ty = Expr.Ty
+
+  type t =
+    | SITyDecl of Ty.Decl.t W.t (** [type bool = true | false] *)
+    | SIExpr of Expr.t W.t (** [5 + 2] *)
+    | SILet of Expr.rec_flag * Expr.value_binding W.t list (** [let rec f = None;;] *)
+  [@@deriving eq, show { with_path = false }]
+end
+
+(********************************Ast********************************)
+
+module Ast
+    (W : Wrap)
+    (SIW : Wrap)
+    (EW : Wrap)
+    (PW : Wrap)
+    (CW : Wrap)
+    (TyExprW : Wrap)
+    (TyDeclW : Wrap) =
+struct
+  module StructureItem = StructureItem (SIW) (EW) (PW) (CW) (TyExprW) (TyDeclW)
+  module Expr = StructureItem.Expr
+  module Pattern = StructureItem.Pattern
+  module Constant = StructureItem.Constant
+  module Ty = StructureItem.Ty
+
+  type t = StructureItem.t W.t list [@@deriving eq, show { with_path = false }]
+end
---
+++ ObaML/lib/parse/navi.ml
@@ -0,0 +1,44 @@
+open Ast
+
+module Location = struct
+  type t =
+    { begin_pos : Lexing.position
+    ; end_pos : Lexing.position
+    }
+
+  let show_lex_pos : Lexing.position -> string =
+    fun { pos_fname = fname; pos_lnum = lnum; pos_bol = bol; pos_cnum = cnum } ->
+    Printf.sprintf "[%s: %d %d %d]" fname lnum bol cnum
+  ;;
+
+  let show loc =
+    let begin_pos = show_lex_pos loc.begin_pos in
+    let end_pos = show_lex_pos loc.end_pos in
+    Printf.sprintf "{begin_pos: %s; end_pos: %s}" begin_pos end_pos
+  ;;
+
+  let pp fmt loc = show loc |> Format.pp_print_string fmt
+
+  let equal { begin_pos = lbp; end_pos = lep } { begin_pos = rbp; end_pos = rep } =
+    lbp = rbp && lep = rep
+  ;;
+
+  let by_begin_end begin_pos end_pos = { begin_pos; end_pos }
+end
+
+module Located : Wrap = struct
+  type 'a t =
+    { data : 'a
+    ; location : Location.t
+    }
+  [@@deriving eq, show { with_path = false }]
+
+  let escape { data; location = _ } = data
+
+  let by_data_begin_end data begin_pos end_pos =
+    { data; location = Location.by_begin_end begin_pos end_pos }
+  ;;
+end
+
+module LocatedAst =
+  Ast (Located) (Located) (Located) (Located) (Located) (Fiction) (Located)
