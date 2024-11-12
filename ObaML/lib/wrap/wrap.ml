49f0093684a73311346c0acc240246d339496c8c
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-11-12 06:05:07 +0300

    Sync ast: Add wrap builder module type.

--- ObaML/lib/wrap/wrap.ml
+++ ObaML/lib/wrap/wrap.ml
@@ -5,3 +5,9 @@ module type T = sig
   val pp : (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a t -> unit
   val show : (Format.formatter -> 'a -> unit) -> 'a t -> string
 end
+
+module Builder (W : T) = struct
+  module type T = sig
+    val wrap : 'a -> Navi.Location.t -> 'a W.t
+  end
+end
